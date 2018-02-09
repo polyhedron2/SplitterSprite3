77b4dd3e49be44c78331e5beaa676591e2c293b1
polyhedron2 <polyhedron2@gmail.com>
2018-02-10 05:48:47 +0900

    XMLからインスタンス生成を行うトレイトを追加

---
+++ src/main/scala/splittersprite3/spawner/Spawner.scala
@@ -0,0 +1,21 @@
+package splittersprite3.spawner
+
+import splittersprite3.spirit.{Spirit}
+
+// XMLファイルを基にインスタンスを生成するトレイト
+trait Spawner[+T] {
+  val spirit: Spirit
+
+  // 処理中に動的に決定される引数の型定義
+  type SpawnArgs
+  // インスタンス生成を行うメソッド
+  protected def _spawn(args: SpawnArgs): T
+  def spawn(args: SpawnArgs) = {
+    _spawn(args)
+  }
+
+  // XMLから読み出すフィールド名・型を取り出すためのフェイク実行用の引数
+  def fakeArgs: SpawnArgs
+  // XMLから読み出すフィールド名・型を取り出すためのフェイク実行
+  lazy val fake = spawn(fakeArgs)
+}
