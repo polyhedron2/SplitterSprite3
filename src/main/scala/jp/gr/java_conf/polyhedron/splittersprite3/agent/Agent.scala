3ea20d430196c3380138bfd9effbbcbc880460c0
polyhedron2 <polyhedron2@gmail.com>
2018-02-23 09:45:18 +0900

    Agentをリファクタリング

--- src/main/scala/jp/gr/java_conf/polyhedron/splittersprite3/agent/Agent.scala
+++ src/main/scala/jp/gr/java_conf/polyhedron/splittersprite3/agent/Agent.scala
@@ -3,9 +3,14 @@ package jp.gr.java_conf.polyhedron.splittersprite3.agent
 class AgentDoublyUsed(a: Agent)
   extends Exception(s"エージェント${a}が多重に起動されました。")
 
+//開始処理と終了処理を定めるシングルトン用のトレイト
 trait Agent {
+  //開始処理
   protected def enter() = {}
+  //終了処理
+  //exOpt: 途中で例外が発生したらSome(ex), なければNone
   protected def exit(exOpt: Option[Exception]) = exOpt.foreach { throw _ }
+
   private var alreadyUsing = false
   def loan(operation: => Any) = synchronized {
     if (alreadyUsing) { throw new AgentDoublyUsed(this) }
@@ -26,8 +31,17 @@ trait Agent {
 }
 
 object Agent {
+  lazy val agents = List(Logger, ThreadPool, Specificator)
+
+  private def _loan(operation: => Any, agentList: List[Agent]) {
+    agentList match {
+      case head :: tail => head.loan { _loan(operation, tail) }
+      case Nil => operation
+    }
+  }
+
   def loan(operation: => Any) = try {
-    Logger.loan { ThreadPool.loan { Specificator.loan { operation } } }
+    _loan(operation, agents)
   } catch {
     case e: Exception => e.printStackTrace()
   }
