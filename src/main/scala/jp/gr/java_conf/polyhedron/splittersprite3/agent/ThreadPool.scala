f8912fc7f7a19b9e6a22ec3576b2f6a2a7058314
polyhedron2 <polyhedron2@gmail.com>
2018-03-10 10:55:54 +0900

    move Runnable trait into ThreadPool

--- src/main/scala/jp/gr/java_conf/polyhedron/splittersprite3/agent/ThreadPool.scala
+++ src/main/scala/jp/gr/java_conf/polyhedron/splittersprite3/agent/ThreadPool.scala
@@ -1,5 +1,6 @@
 package jp.gr.java_conf.polyhedron.splittersprite3.agent
 
+import java.lang.{Runnable => JRunnable}
 import java.lang.{Thread => JThread}
 
 import jp.gr.java_conf.polyhedron.splittersprite3.common
@@ -8,7 +9,7 @@ object ThreadPool extends LoanAgent {
   private var runningThreads = Set[Thread]()
 
   //スレッドオブジェクトを２回スタートしないために開始後に終了用関数のみ返す
-  def startAndGetHalter(runnable: common.Runnable) {
+  def startAndGetHalter(runnable: Runnable) {
     synchronized {
       val thread = new Thread(runnable)
       add(thread)
@@ -33,12 +34,37 @@ object ThreadPool extends LoanAgent {
     exOpt.foreach { throw _ }
   }
 
-  private class Thread(runnable: common.Runnable) extends JThread(runnable) {
+  private class Thread(runnable: Runnable) extends JThread(runnable) {
     override def run() {
       super.run()
       remove(this)
     }
 
     def halt() { runnable.halt() }
+  }
+
+  //スレッド実行対象トレイト
+  trait Runnable extends JRunnable {
+    private var done = false
+    //停止用メソッド
+    def halt(): Unit = synchronized { done = true }
+
+    override def run() {
+      val exOpt = try {
+        enter()
+        while (!done && runOnce()) { }
+        None
+      } catch {
+        case e: Exception => Some(e)
+      }
+      exit(exOpt)
+    }
+
+    //処理開始時に一度だけ呼ばれるメソッド
+    def enter() { }
+    //処理終了時に一度だけ呼ばれるメソッド, 例外があっても実行される
+    def exit(exOpt: Option[Exception]): Unit = exOpt.foreach { throw _ }
+    //ループされる処理　戻り値はループを継続するか否か
+    def runOnce(): Boolean
   }
 }
